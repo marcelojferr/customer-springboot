0ec6750075318e086eda665d9c72dcab68e2517b
POSITIVO <POSITIVO@DESKTOP-MBGLGHO>
2024-06-19 17:54:14 -0300

    Spring - Aplicacao inicial

---
+++ src/test/java/com/javaproject/customer_springboot/CustomerSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaproject.customer_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
